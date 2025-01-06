265da7a676de64f7e982233ea0776636f3bc9fcd
ju__ni <89272299+JulianDKal@users.noreply.github.com>
2025-01-06 21:01:16 +0100

    parrel sync and tests

---
+++ Assets/Scripts/ObjectDisabler.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using Unity.Netcode;
+
+public class ObjectDisabler : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
