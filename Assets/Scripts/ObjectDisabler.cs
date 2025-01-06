c8169d0b4403559291c1d813b07a7d7ece5922de
ju__ni <89272299+JulianDKal@users.noreply.github.com>
2025-01-06 23:17:28 +0100

    basic object disabling

--- Assets/Scripts/ObjectDisabler.cs
+++ Assets/Scripts/ObjectDisabler.cs
@@ -1,17 +1,47 @@
+using System.Collections.Generic;
 using UnityEngine;
 using Unity.Netcode;
+using UnityEngine.SceneManagement;
 
-public class ObjectDisabler : MonoBehaviour
+public class ObjectDisabler : NetworkBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Awake()
     {
-        Debug.Log();
+        Debug.Log("ObjectDisabler Awake");
     }
 
     // Update is called once per frame
-    void Update()
+    public override void OnNetworkSpawn()
     {
-        
+        Debug.Log("||||||||||| ObjectDisabler OnNetworkSpawned for " + NetworkManager.Singleton.LocalClientId);
+        if(NetworkManager.Singleton.LocalClientId == 0) DisableObjectsFromPlayer2();
+        else DisableObjectsFromPlayer1();
+        base.OnNetworkSpawn();
+    }
+
+    private void DisableObjectsFromPlayer2()
+    {
+        List<GameObject> rootObjects = new List<GameObject>();
+        SceneManager.GetActiveScene().GetRootGameObjects(rootObjects);
+        foreach (var obj in rootObjects)
+        {
+            if (obj.CompareTag("Player2"))
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+    private void DisableObjectsFromPlayer1()
+    {
+        List<GameObject> rootObjects = new List<GameObject>();
+        SceneManager.GetActiveScene().GetRootGameObjects(rootObjects);
+        foreach (var obj in rootObjects)
+        {
+            if (obj.CompareTag("Player1"))
+            {
+                obj.SetActive(false);
+            }
+        }
     }
 }
