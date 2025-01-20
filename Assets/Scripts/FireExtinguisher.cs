a3f96c21d3ad751b97bbefa556359f559658fa64
BlackCube <blackcube4@gmail.com>
2025-01-21 00:38:44 +0100

    fire extinguisher

--- Assets/Scripts/FireExtinguisher.cs
+++ Assets/Scripts/FireExtinguisher.cs
@@ -1,18 +1,38 @@
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class FireExtinguisher : MonoBehaviour
 {
     public ParticleSystem particleSystem;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // XRGrabInteractable grabbable = GetComponent<XRGrabInteractable>();
+        // grabbable.activated.AddListener(EnableParticles);
+        // grabbable.deactivated.AddListener(DisableParticles);
+        particleSystem.Stop();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void EnableParticles(ActivateEventArgs arg)
+    {
+        // if (!particleSystem.isPlaying)
+        // {
+            particleSystem.Play();
+        // }
+    }
+
+    public void DisableParticles(DeactivateEventArgs arg)
+    {
+        // if (particleSystem.isPlaying)
+        // {
+            particleSystem.Stop();
+        // }
     }
 }
