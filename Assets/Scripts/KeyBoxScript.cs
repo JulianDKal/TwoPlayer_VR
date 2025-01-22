f7ea900bbc3e20b24bf414b88f0d9d4d9a497046
BlackCube <blackcube4@gmail.com>
2025-01-22 06:57:41 +0100

    only music and gaze to go

--- Assets/Scripts/KeyBoxScript.cs
+++ Assets/Scripts/KeyBoxScript.cs
@@ -1,8 +1,14 @@
 using UnityEngine;
-
 public class KeyBoxScript : MonoBehaviour
 {
     public GameObject key;
+    public Transform lidTransform;
+    public Collider lidCollider;
+    public float openSpeed = 50f;
+    public float openAngle = -95f;
+    public bool isOpening = false;
+    private float currentAngle = 0f;
+
     void Start()
     {
         EventManager.instance.puzzleOneCompletedEvent += DisableMe;
@@ -11,14 +17,36 @@ public class KeyBoxScript : MonoBehaviour
         foreach (BoxCollider keyCollider in colliders)
         {
             Physics.IgnoreCollision(myCollider, keyCollider);
-            //Debug.Log($"Found BoxCollider: {collider}");
         }
-        
     }
 
-    void DisableMe()
+    void Update()
     {
-        this.gameObject.SetActive(false);
+        if (isOpening)
+        {
+            // Berechnet die zu rotierende Menge für diesen Frame
+            float rotationStep = openSpeed * Time.deltaTime;
+            
+            // Stoppt bei der maximalen Öffnung
+            if (currentAngle + rotationStep > Mathf.Abs(openAngle))
+            {
+                rotationStep = Mathf.Abs(openAngle) - currentAngle;
+                isOpening = false;
+            }
+            
+            // Rotiert den Deckel um die Z-Achse
+            lidTransform.Rotate(0f, 0f, -rotationStep);
+            currentAngle += rotationStep;
+        }
+    }
+
+    public void DisableMe()
+    {
+        if (lidCollider != null)
+        {
+            lidCollider.enabled = false;
+        }
+        isOpening = true;
         EventManager.instance.puzzleOneCompletedEvent -= DisableMe;
     }
 }
